74520462e6c3259915122f3948ffd3d949270c9e
zhanghuanfu <14126181@bjtu.edu.cn>
2018-11-23 14:26:16 +0800

    Update JenkinsMain.java

--- JenkinsMain.java
+++ JenkinsMain.java
@@ -5,7 +5,7 @@ public class JenkinsMain {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("Zhanghf Jenkins Setup ！");
+		System.out.println("Zhanghf Jenkins Setup !");
 	}
 
 }
