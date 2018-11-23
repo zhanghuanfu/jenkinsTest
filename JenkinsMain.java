bf5acda6867b4c725aa6166fb1eb9b6ac7e93af7
zhanghuanfu <14126181@bjtu.edu.cn>
2018-11-23 14:20:45 +0800

    Add files via upload

---
+++ JenkinsMain.java
@@ -0,0 +1,11 @@
+package beforetest;
+
+public class JenkinsMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Zhanghf Jenkins Setup ！");
+	}
+
+}
